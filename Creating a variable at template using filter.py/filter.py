d3a67e4b78634a87f9fd5c06d4d4b63a1198f5da
Yoo In Keun <keun0390@naver.com>
2019-01-31 18:02:27 +0900

    템플릿에서 변수 생성 가능하게 하는 방법
    Class and function enabling create a variable at template

---
+++ Creating a variable at template using filter.py/filter.py
@@ -0,0 +1,62 @@
+from django import template
+
+register = template.Library()
+
+# 문자열 변수 생성 가능
+
+class SetVarNode(template.Node):
+    def __init__(self, new_val, var_name):
+        self.new_val = new_val
+        self.var_name = var_name
+    def render(self, context):
+        context[self.var_name] = self.new_val
+        return ''
+
+import re
+
+@register.tag
+
+def setvar(parser,token):
+    # This version uses a regular expression to parse tag contents.
+    try:
+        # Splitting by None == splitting by spaces.
+        tag_name, arg = token.contents.split(None, 1)
+    except ValueError:
+        raise template.TemplateSyntaxError("%r tag requires arguments" % token.contents.split()[0])
+    m = re.search(r'(.*?) as (\w+)', arg)
+    if not m:
+        raise template.TemplateSyntaxError("%r tag had invalid arguments" % tag_name)
+    new_val, var_name = m.groups()
+    if not (new_val[0] == new_val[-1] and new_val[0] in ('"', "'")):
+        raise template.TemplateSyntaxError("%r tag's argument should be in quotes" % tag_name)
+    return SetVarNode(new_val[1:-1], var_name)
+
+
+# 모든 타입 변수 생성 가능
+
+class SetVarNode(template.Node):
+
+    def __init__(self, var_name, var_value):
+        self.var_name = var_name
+        self.var_value = var_value
+
+    def render(self, context):
+        try:
+            value = template.Variable(self.var_value).resolve(context)
+        except template.VariableDoesNotExist:
+            value = ""
+        context[self.var_name] = value
+
+        return u""
+
+@register.tag(name='set')
+
+def set_var(parser, token):
+    """
+    {% set some_var = '123' %}
+    """
+    parts = token.split_contents()
+    if len(parts) < 4:
+        raise template.TemplateSyntaxError("'set' tag must be of the form: {% set <var_name> = <var_value> %}")
+
+    return SetVarNode(parts[1], parts[3])
